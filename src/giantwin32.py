b19d2391d1427beaac0b195e9d18c481eca1c08a
Pranav Salunke <pps.pranav@gmail.com>
2013-03-14 04:40:24 +0530

    Changed Code Structure - Functionality is same
    Well, I have removed some repetative lines by making a function for the
    same. This function will be addressing the keyboard calls and also will
    make the code more pythonic. :D

--- src/giantwin32.py
+++ src/giantwin32.py
@@ -198,341 +198,176 @@ def release(*args):
     '''
     for i in args:
            win32api.keybd_event(VK_CODE[i],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+
+
+def event(code1):
+    '''
+        this function will call the win32 api keyboard event,
+        basically this method will reduce the LOC's by
+        generalizing the calls to simplify typer(xx,xx) function.
+    '''
+    win32api.keybd_event(VK_CODE[code1], 0,0,0)
+    time.sleep(.05)
+    win32api.keybd_event(VK_CODE[code1],0 ,win32con.KEYEVENTF_KEYUP ,0)
+
+
+def event(code1,code2):
+    '''
+        This is overloaded event() funtion - where in there are two event calls instead of one !!!
+        
+        this function will call the win32 api keyboard event,
+        basically this method will reduce the LOC's by
+        generalizing the calls to simplify typer(xx,xx) function.
+    '''
+    win32api.keybd_event(VK_CODE[code1], 0,0,0)
+    win32api.keybd_event(VK_CODE[code2], 0,0,0)
+    time.sleep(.05)
+    win32api.keybd_event(VK_CODE[code1],0 ,win32con.KEYEVENTF_KEYUP ,0)
+    win32api.keybd_event(VK_CODE[code2],0 ,win32con.KEYEVENTF_KEYUP ,0)
+
  
 def typer(string=None,*args):
 ##    time.sleep(4)
     for i in string:
         if i == ' ':
-            win32api.keybd_event(VK_CODE['spacebar'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['spacebar'],0 ,win32con.KEYEVENTF_KEYUP ,0)
+            event('spacebar')
  
         elif i == '!':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['1'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['1'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+            event('left_shift','1')
+
         elif i == '@':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['2'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['2'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','2')
+           
         elif i == '{':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['['], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['['],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','[')
+           
         elif i == '?':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['/'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['/'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','/')
+           
         elif i == ':':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE[';'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE[';'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift',';')
+           
         elif i == '"':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['\''], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['\''],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','\\')
+                 
         elif i == '}':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE[']'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE[']'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift',']')
+            
         elif i == '#':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['3'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['3'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','3')
+     
         elif i == '$':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['4'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['4'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','4')
+      
         elif i == '%':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['5'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['5'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','5')
+                 
         elif i == '^':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['6'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['6'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','6')
+        
         elif i == '&':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['7'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['7'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','7')            
+      
         elif i == '*':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['8'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['8'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','8')            
+           
         elif i == '(':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['9'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['9'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','9')
+        
         elif i == ')':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['0'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['0'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','0')
+        
         elif i == '_':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['-'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['-'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
- 
+           event('left_shift','-')
+             
         elif i == '=':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['+'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['+'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','+')
+   
         elif i == '~':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['`'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['`'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','`')
+         
         elif i == '<':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE[','], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE[','],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift',')')
+       
         elif i == '>':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['.'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['.'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','.')
+         
         elif i == 'A':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['a'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['a'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','a')
+      
         elif i == 'B':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['b'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['b'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','b')
+            
         elif i == 'C':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['c'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['c'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','c')
+            
         elif i == 'D':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['d'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['d'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','d')
+          
         elif i == 'E':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['e'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['e'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','e')
+            
         elif i == 'F':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['f'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['f'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','f')
+            
         elif i == 'G':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['g'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['g'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','g')
+            
         elif i == 'H':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['h'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['h'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','h')
+        
         elif i == 'I':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['i'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['i'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','i')
+        
         elif i == 'J':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['j'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['j'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','j')
+     
         elif i == 'K':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['k'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['k'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','k')
+     
         elif i == 'L':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['l'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['l'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','l')
+      
         elif i == 'M':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['m'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['m'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','m')
+       
         elif i == 'N':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['n'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['n'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'O':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['o'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['o'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'P':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['p'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['p'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'Q':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['q'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['q'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'R':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['r'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['r'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'S':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['s'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['s'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'T':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['t'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['t'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'U':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['u'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['u'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'V':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['v'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['v'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'W':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['w'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['w'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'X':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['x'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['x'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'Y':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['y'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['y'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
-        elif i == 'Z':
-            win32api.keybd_event(VK_CODE['left_shift'], 0,0,0)
-            win32api.keybd_event(VK_CODE['z'], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE['left_shift'],0 ,win32con.KEYEVENTF_KEYUP ,0)
-            win32api.keybd_event(VK_CODE['z'],0 ,win32con.KEYEVENTF_KEYUP ,0)
- 
+           event('left_shift','n')
     
-        else:    
-            win32api.keybd_event(VK_CODE[i], 0,0,0)
-            time.sleep(.05)
-            win32api.keybd_event(VK_CODE[i],0 ,win32con.KEYEVENTF_KEYUP ,0)
+        elif i == 'O':
+           event('left_shift','o')
+      
+        elif i == 'P':
+           event('left_shift','p')
+            
+        elif i == 'Q':
+           event('left_shift','q')
+           
+        elif i == 'R':
+           event('left_shift','r')
+  
+        elif i == 'S':
+           event('left_shift','s')
+        
+        elif i == 'T':
+           event('left_shift','t')
+        elif i == 'U':
+           event('left_shift','u')
+            
+        elif i == 'V':
+           event('left_shift','v')
+        elif i == 'W':
+           event('left_shift','w')
+            
+        elif i == 'X':
+           event('left_shift','x')
+           
+        elif i == 'Y':
+           event('left_shift','y')
+       
+        elif i == 'Z':
+           event('left_shift','z')
+           
+        else:
+            event(i)
+
